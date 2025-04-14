329a385e817dc8587fb1dfe0061e93d273d401bf
Dana Goosen <klymentovska@gmail.com>
2025-04-14 07:58:46 +0200

    Placing surfaces via rects

--- code/main.py
+++ code/main.py
@@ -17,7 +17,17 @@ n_of_stars = 20
 
 # importing an image
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
+
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(n_of_stars)]
+
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
     # event loop
@@ -27,12 +37,18 @@ while running:
 
     # draw the game
     display_surface.fill('darkgray')
-    x += 0.1
 
     for i in range(n_of_stars):
-        star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
         display_surface.blit(star_surf, star_positions[i])
+        
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
 
-    display_surface.blit(player_surf, (x,150))
+    player_rect.x += player_direction
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
+
 pygame.quit()
