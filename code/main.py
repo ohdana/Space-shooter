9c5d6ff9be12058c94a6096e7daf32907f2a1007
Dana Goosen <klymentovska@gmail.com>
2025-04-14 10:16:50 +0200

    Movement & delta time

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
@@ -18,7 +19,8 @@ n_of_stars = 20
 # importing an image
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = 1
+player_direction = pygame.math.Vector2(1, 0)
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(n_of_stars)]
@@ -30,6 +32,8 @@ laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    dt = clock.tick() / 1000
+
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,17 +42,16 @@ while running:
     # draw the game
     display_surface.fill('darkgray')
 
-    for i in range(n_of_stars):
-        display_surface.blit(star_surf, star_positions[i])
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
         
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
 
-    player_rect.x += player_direction
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
-
+    # player movement
+    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf, player_rect)
+
     pygame.display.update()
 
 pygame.quit()
